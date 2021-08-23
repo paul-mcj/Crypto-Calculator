63c0944ffbc64d5bab44aabe3dab12d0d5658dd0
paul-mcj <83841390+paul-mcj@users.noreply.github.com>
2021-08-23 18:21:02 -0400

    Add files via upload
    - more contrast: changed background-color of container to lighter white so text is more visible
    - when returning amount, place the appropriate symbol depending on the currency used
    - added alert for amount input (cannot be less than zero, and must be a number)
    - reset the amount field when the submit button is clicked and the value is accepted
    - updated utility class for text-aligning multiple elements
    - updated some html id's to not useCamelCase, but use-dashes-instead
    - added a loading animation when gathering data from the server (for UX)

--- crypto.js
+++ crypto.js
@@ -1,3 +1,7 @@
+// todo:
+// - update to use es6 classes and constructors
+// - update from xhr model of async to es6 promises
+
 // function httpReq() {
 //     this.request = new XMLHttpRequest();
 // }
@@ -34,23 +38,83 @@
 //         console.log(error);
 //     });
 
+const form = document.querySelector(".form");
+const cryptoAmount = document.querySelector("#crypto-amount");
+const cryptoType = document.querySelector("#crypto-type");
+const fiatCurrency = document.querySelector("#fiat-currency");
+const loader = document.querySelector(".loader");
 const submit = document.querySelector(".submit");
 const result = document.querySelector(".result");
-const form = document.querySelector(".form");
-const serverTime = document.querySelector(".serverTime");
-const cryptoType = document.querySelector("#cryptoType");
-const cryptoAmount = document.querySelector("#cryptoAmount");
-const fiatCurrency = document.querySelector("#fiatCurrency");
+const serverTime = document.querySelector(".server-time");
 
 form.addEventListener("submit", function (event) {
     getPrice();
-    getServerTime();
     event.preventDefault();
 });
 
-//todo: need to fix??
 submit.addEventListener("click", getPrice);
-submit.addEventListener("click", getServerTime);
+
+function getPrice() {
+    const xhr = new XMLHttpRequest();
+    let amount = cryptoAmount.value;
+    let crypto = cryptoType.value;
+    let fiat = fiatCurrency.value;
+    if (amount <= 0 || isNaN(amount)) {
+        window.alert("Amount must be greater than 0. Try again!");
+    } else {
+        xhr.open("get", `https://api.coinbase.com/v2/prices/${crypto}-${fiat}/spot`, true);
+        xhr.onload = function () {
+            if (this.status === 200) {
+                let response = JSON.parse(this.responseText);
+                let newAmount = Number(response.data.amount * amount).toFixed(2);
+                function numberWithCommas(amount) {
+                    return amount.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+                }
+                let symbol;
+                switch (fiat) {
+                    case "gbp":
+                        symbol = "\u00a3";
+                        break;
+                    case "eur":
+                        symbol = "\u20ac";
+                        break;
+                    case "jpy":
+                        symbol = "\u00a5";
+                        break;
+                    case "cny":
+                        symbol = "\u00a5";
+                        break;
+                    default:
+                        symbol = "\u0024";
+                        break;
+                }
+                processingData();
+                setTimeout(revealingData, 3000);
+                result.textContent = `${amount} ${crypto.toUpperCase()} is worth ${symbol}${numberWithCommas(
+                    newAmount
+                )} in ${fiat.toUpperCase()}`;
+            } else {
+                window.alert("Something when wrong with retrieving data");
+            }
+        };
+        xhr.send();
+        cryptoAmount.value = "";
+    }
+}
+
+function processingData() {
+    loader.style.display = "block";
+    submit.style.display = "none";
+    result.style.display = "none";
+    serverTime.textContent = `Fetching data from server...`;
+}
+
+function revealingData() {
+    loader.style.display = "none";
+    submit.style.display = "block";
+    result.style.display = "block";
+    getServerTime();
+}
 
 function getServerTime() {
     const xhr = new XMLHttpRequest();
@@ -58,7 +122,6 @@ function getServerTime() {
     xhr.onload = function () {
         if (this.status === 200) {
             let response = JSON.parse(this.responseText);
-            console.log(response);
             serverTime.textContent = `Data retrieved from server at ${response.data.iso}`;
         } else {
             console.log("Time error");
@@ -66,33 +129,3 @@ function getServerTime() {
     };
     xhr.send();
 }
-
-function getPrice(event) {
-    const xhr = new XMLHttpRequest();
-    let crypto = cryptoType.value;
-    let amount = cryptoAmount.value;
-    let fiat = fiatCurrency.value;
-    xhr.open("get", `https://api.coinbase.com/v2/prices/${crypto}-${fiat}/spot`, true);
-    xhr.onload = function () {
-        if (this.status === 200) {
-            let response = JSON.parse(this.responseText);
-            let newAmount = Number(response.data.amount * amount).toFixed(2);
-            function numberWithCommas(amount) {
-                return amount.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-            }
-            result.textContent = `${amount} ${crypto.toUpperCase()} is worth ${numberWithCommas(
-                newAmount
-            )} in ${fiat}`;
-        } else {
-            console.log("Error");
-        }
-    };
-    xhr.send();
-}
-
-// todo:
-// - add alerts for amount input (cannot be less than zero, and must be a number)
-// - reset the amount field when the submit button is clicked
-// - add a gif to "calculate" when the button is clicked for UI
-// - update to use es6 classes and constructors
-// - update from xhr model of async to es6 promises
