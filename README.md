8e63b8ae316d740bc8b9b0eafecf3e22313225e6
paul-mcj <83841390+paul-mcj@users.noreply.github.com>
2021-08-31 18:01:54 -0400

    Add files via upload
    All files
    - Updated comments to help organize code and give insight by adding a table of contents and labeling each major section and individual ruleset/function
    
    README
    - Updated the README.md to include what I was thinking when I made this project, including things I learned and challenges I overcame

--- README.md
+++ README.md
@@ -1,4 +1,61 @@
-# Crypto-Calculator
-This app works with the Coinbase open API, where it allows a user to convert any amount of a chosen crypto (BTC, ETH, LTC, BCH) into its worth in some fiat currency. All prices use Coinbase's "spot price" in real time, which means it gathers the current market price for a cryptocurrency. A returned server time is given to inform the user that the price is current as well.
+# Crypto Calculator (v1.0.6)
 
-Please visit the [Crypto Calculator](https://paul-mcj.github.io/Crypto-Calculator/) for yourself!
+## About this project
+
+### What does it do?
+
+By using the Coinbase API, users get to choose an amount of money in fiat currency (there are about 7 total currencies to choose from) and a crypto currency of their choice (of which there are four). The application will calculate how many coins the amount of fiat currency is worth.
+
+Additionally, it gathers the server time when a request us made to the API and displays it so that the user knows the result is accurate and recent
+
+### Why do it?
+
+After learning about the XMLHttpRequest object, I decided I wanted to make a small application that integrates with an API by gathering real-time data. After a few versions, it was updated to use the Fetch API instead.
+
+I choose a crypto API since I find crypto to be an interesting topic, but also because my best friends loves crypto and I wanted to make him something he could use from all of the web development knowledge I have accumulated!
+
+## Challenges and what I learned
+
+I knew immediately that this project was not going to be a platform to exchange, convert, but or sell currency. But, I knew that it was possible to gather data through an API and so something with it. That being said, this smaller-sized application still has a few challenges:
+
+-   Through some research, I realized that an <input> element in html should have a "for" attribute the exact same as the related element in order to properly bind them together. This was needed specifically for some JS query selection, to make sure values were properly gathered for processing.
+-   I started to really understand the importance of other attributes, too like why its nice to have a "type = number" attribute for an <input> tag that should be immediately taking in a numbered value -- it helps with UX/UI a lot!
+-   I learnt how to apply multiple cases to a single outcome in a switch/case statement. This helps make it look a lot nicer for something like returning the same symbol for both Chinese yeun and Japanese yen:
+
+```js
+case "eur":
+    symbol = "\u20ac";
+    break;
+case "jpy":
+case "cny":
+    symbol = "\u00a5";
+    break;
+```
+
+-   I also had a bit of trouble getting the correct response data back from the API. After some trial-and-error, I found that I wasn't properly referencing the variable. I wouldn't return a valid value with:
+
+```js
+let newAmount = Number(data.amount * amount).toFixed(2);
+```
+
+-   But I did with this:
+
+```js
+let newAmount = Number(data.data.amount * amount).toFixed(2);
+```
+
+-   I also did some basic research to figure out how to add some commas in the returned value if it larger than 1000 fiat. It was a returned amount combined with a regular expression:
+
+```js
+numberWithCommas = (amount) => amount.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+```
+
+## Conclusions
+
+This app was a lot of fun and it took me a few days to make. I'm glad I was able to use my knowledge of Fetch API and apply it to something real! I'm excited to apply my knowledge to bigger and better projects in the future :)
+
+## Author
+
+-   LinkedIn: [Paul McJannet](www.linkedin.com/in/paul-mcjannet)
+-   Frontend Mentor: [@paul-mcj](https://www.frontendmentor.io/profile/paul-mcj)
+-   Github: [paul-mcj](https://github.com/paul-mcj)
--- crypto-1.js
+++ crypto-1.js
@@ -1,3 +1,25 @@
+/*JS file for Crypto Calculator "index.html"*/
+
+/******************************************************************************
+	Table of Contents
+	-----------------
+	1. Global Variables
+
+	2. Event Listeners
+        2.1 -- form.addEventListener("submit", (event))
+        2.2 -- submit.addEventListener("click", (event))
+
+	3. Global Functions
+        3.1 -- getPrice()
+        3.2 -- processingData()
+        3.3 -- revealingData()
+        3.4 -- getServerTime()
+        
+*******************************************************************************/
+
+/******************************************************************************
+1. Global Variables
+******************************************************************************/
 const form = document.querySelector(".form");
 const cryptoAmount = document.querySelector("#crypto-amount");
 const cryptoType = document.querySelector("#crypto-type");
@@ -7,27 +29,44 @@ const submit = document.querySelector(".submit");
 const result = document.querySelector(".result");
 const serverTime = document.querySelector(".server-time");
 
+/******************************************************************************
+2. Event Listeners
+******************************************************************************/
+
+// 2.1 This listens on the form element for a submission of user input, and calls the function getPrice() to output data.
 form.addEventListener("submit", (event) => {
     getPrice();
     event.preventDefault();
 });
 
+// 2.2 A similar function of the above event listener is included here but occurs when the button is specifically clicked.
 submit.addEventListener("click", (event) => getPrice(event));
 
+/******************************************************************************
+3. Global Functions
+******************************************************************************/
+
+// 3.1 This function performs the main logic of the app, including: gathering all user data, checking input is valid, fetching data from the API, processing the data, revealing the data, and resetting the input value.
 function getPrice() {
+    // gather user input values
     let amount = cryptoAmount.value;
     let crypto = cryptoType.value;
     let fiat = fiatCurrency.value;
+    //check amount is a valid number
     if (amount <= 0 || isNaN(amount)) {
         window.alert("Amount must be greater than 0. Try again!");
     } else {
+        //fetch data from the API, passing in user inputs to grab the exact conversion types
         fetch(`https://api.coinbase.com/v2/prices/${crypto}-${fiat}/spot`)
             .then((response) => response.json())
             .then((data) => {
+                // round the data to hundredths
                 let newAmount = Number(data.data.amount * amount).toFixed(2);
+                // and make sure it is separated into thousands with commas for easier readability (if the returned value is larger than 999.99).
                 numberWithCommas = (amount) =>
                     amount.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
                 let symbol;
+                // set the fiat symbol according to the user input
                 switch (fiat) {
                     case "gbp":
                         symbol = "\u00a3";
@@ -43,16 +82,20 @@ function getPrice() {
                         symbol = "\u0024";
                 }
                 processingData();
+                //purposefully wait two seconds to return data for UI/UX (a loader animation is used in this timeframe)
                 setTimeout(revealingData, 2000);
+                //set the amount to be displayed on the webpage
                 result.textContent = `${amount} ${crypto.toUpperCase()} is worth ${symbol}${numberWithCommas(
                     newAmount
                 )} in ${fiat.toUpperCase()}`;
             })
             .catch((error) => console.log(error));
     }
+    //reset the value input field
     cryptoAmount.value = "";
 }
 
+// 3.2 When this function is called the loader is shown while hiding the result amount and calculate button
 processingData = () => {
     loader.style.display = "block";
     submit.style.display = "none";
@@ -60,6 +103,7 @@ processingData = () => {
     serverTime.textContent = `Fetching data from server...`;
 };
 
+// 3.3 This removes the loader and displays the conversion result to the user
 revealingData = () => {
     loader.style.display = "none";
     submit.style.display = "block";
@@ -67,6 +111,7 @@ revealingData = () => {
     getServerTime();
 };
 
+// 3.4  Fetches the API server time and displays it to the user so they know data is recent
 getServerTime = () => {
     fetch("https://api.coinbase.com/v2/time")
         .then((response) => response.json())
